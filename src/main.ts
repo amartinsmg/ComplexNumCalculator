d5ff76823a8299f755e3c4570893e45e384dd1c3
amartinsmg <andremindurimg@gmail.com>
2022-07-12 16:55:53 -0300

    Adding babel and webpack

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import "./style.css";
+
 type cnumFunction = (
   x_real: number,
   x_imag: number,
@@ -13,27 +15,81 @@ interface ProgramWasm {
   cdiv: cnumFunction;
 }
 
-async function main() {
+declare const MathJax: any;
+
+function formatComplex(creal: number, cimag: number): string {
+  if (creal === 0) {
+    return cimag === 0 ? "0" : `${cimag}i`;
+  } else {
+    if (cimag === 0) return `${creal}`;
+    else if (cimag > 0) return `${creal} + ${cimag}i`;
+    else return `${creal} ${cimag}i`;
+  }
+}
+
+async function main(): Promise<void> {
   const WasmInstance = await WebAssembly.instantiateStreaming(
       await fetch("assets/program.wasm")
     ),
+    Form = document.querySelector("#operands-form") as HTMLFormElement,
+    //@ts-ignore
+    Inputs = Array.from(
+      document.querySelectorAll(".operand-input")
+    ) as HTMLInputElement[],
+    OutputEl = document.querySelector("#output-data") as HTMLOutputElement,
     { memory, cadd, csub, cmul, cdiv }: ProgramWasm = WasmInstance.instance
       .exports as any,
-    getArr = (byteOffset: number): number[] => {
+    getArr = (byteOffset: number): [number, number] => {
       const FloatArr = new Float64Array(memory.buffer, byteOffset, 2),
         //@ts-ignore
-        Arr = Array.from<number>(FloatArr);
+        Arr = Array.from<number>(FloatArr) as [number, number];
       return Arr;
     };
 
-  // let x: number[] = getArr(cadd(2, 3, 5, -1));
-  // console.log(x);
-  // x = getArr(csub(2, 3, 5, -1));
-  // console.log(x);
-  // x = getArr(cmul(2, 3, 5, -1));
-  // console.log(x);
-  // x = getArr(cdiv(2, 3, 5, -1));
-  // console.log(x);
+  Inputs.forEach((el, i, arr) => {
+    el.addEventListener("keydown", (e) => {
+      const nextEl = arr[i + 1];
+      if (e.key === "Enter" && nextEl) {
+        e.preventDefault();
+        nextEl.value = "";
+        nextEl.focus();
+      }
+    });
+  });
+
+  Form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    OutputEl.innerHTML = "";
+    const Data = new FormData(Form),
+      Operands = Inputs.map((el) => parseFloat(el.value)) as [
+        number,
+        number,
+        number,
+        number
+      ],
+      OPCODE = (Data.get("operation") as FormDataEntryValue).toString();
+    let result: number;
+    switch (OPCODE) {
+      case "add":
+        result = cadd(...Operands);
+        break;
+      case "sub":
+        result = csub(...Operands);
+        break;
+      case "mul":
+        result = cmul(...Operands);
+        break;
+      case "div":
+        result = cdiv(...Operands);
+        break;
+      default:
+        throw "Invalid option";
+    }
+    const ResultArr = getArr(result),
+      RESULT_STR: string = formatComplex(...ResultArr),
+      Svg: HTMLElement = MathJax.tex2svg(RESULT_STR);
+    OutputEl.appendChild(Svg);
+  });
 }
 
-main();
+window.onload = main;
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const CssMinimizerWebpack = require("css-minimizer-webpack-plugin"),
+  CopyWebpackPlugin = require("copy-webpack-plugin"),
+  MiniCssExtractPlugin = require("mini-css-extract-plugin"),
+  path = require("path"),
+  TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = {
+  entry: "./src/main.ts",
+  output: {
+    filename: "assets/bundle.js",
+    path: path.resolve(__dirname, "build"),
+  },
+  mode: "production",
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "build"),
+    },
+    port: 8080,
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: ["babel-loader", "ts-loader"],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.js$/,
+        use: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js", ".css"],
+  },
+  optimization: {
+    minimizer: [new TerserPlugin(), new CssMinimizerWebpack()],
+  },
+  plugins: [
+    new CopyWebpackPlugin({ patterns: ["src/index.html"] }),
+    new MiniCssExtractPlugin({
+      filename: "assets/bundle.css",
+    }),
+  ],
+};
