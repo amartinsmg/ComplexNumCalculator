dbd24cbd30ac95835b563ca150e398aa0a6b7ad2
amartinsmg <andremindurimg@gmail.com>
2022-09-16 11:50:03 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -36,68 +36,77 @@ function formatComplex(creal: number, cimag: number): string {
 // and instantiating the wasm module and calling its functions when the form is submitted.
 
 async function main(): Promise<void> {
-  const WasmInstance = await WebAssembly.instantiateStreaming(
-      await fetch("assets/program.wasm")
-    ),
-    Form = document.querySelector("#operands-form") as HTMLFormElement,
+  const Form = document.querySelector("#operands-form") as HTMLFormElement,
     //@ts-ignore
     Inputs = Array.from(
       document.querySelectorAll(".operand-input")
     ) as HTMLInputElement[],
     OpSelect = document.querySelector("#opcode") as HTMLSelectElement,
-    OutputEl = document.querySelector("#output-data") as HTMLOutputElement,
-    { memory, cadd, csub, cmul, cdiv }: ProgramWasm = WasmInstance.instance
-      .exports as any,
-    getArr = (byteOffset: number): [number, number] => {
-      const FloatArr = new Float64Array(memory.buffer, byteOffset, 2),
-        //@ts-ignore
-        Arr = Array.from<number>(FloatArr) as [number, number];
-      return Arr;
-    };
+    OutputEl = document.querySelector("#output-data") as HTMLOutputElement;
 
-  Inputs.forEach((el, i, arr) => {
-    el.addEventListener("keydown", (e) => {
-      const nextEl = arr[i + 1];
-      if (e.key === "Enter" && nextEl) {
-        e.preventDefault();
-        nextEl.value = "";
-        nextEl.focus();
+  try {
+    const WasmModule = await WebAssembly.instantiateStreaming(
+        await fetch("assets/program.wasm")
+      ),
+      { memory, cadd, csub, cmul, cdiv }: ProgramWasm = WasmModule.instance
+        .exports as any,
+      getArr = (byteOffset: number): [number, number] => {
+        const FloatArr = new Float64Array(memory.buffer, byteOffset, 2),
+          //@ts-ignore
+          Arr = Array.from<number>(FloatArr) as [number, number];
+        return Arr;
+      };
+
+    Inputs.forEach((el, i, arr) => {
+      el.addEventListener("keydown", (e) => {
+        const nextEl = arr[i + 1];
+        if (e.key === "Enter" && nextEl) {
+          e.preventDefault();
+          nextEl.value = "";
+          nextEl.focus();
+        }
+      });
+    });
+
+    Form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      try {
+        OutputEl.innerHTML = "";
+        const Operands = Inputs.map((el) => parseFloat(el.value)) as [
+            number,
+            number,
+            number,
+            number
+          ],
+          OPCODE = OpSelect.value;
+        let result: number;
+        switch (OPCODE) {
+          case "add":
+            result = cadd(...Operands);
+            break;
+          case "sub":
+            result = csub(...Operands);
+            break;
+          case "mul":
+            result = cmul(...Operands);
+            break;
+          case "div":
+            result = cdiv(...Operands);
+            break;
+          default:
+            throw "Invalid option";
+        }
+        const ResultArr = getArr(result),
+          RESULT_STR: string = formatComplex(...ResultArr),
+          Svg: HTMLElement = MathJax.tex2svg(RESULT_STR);
+        OutputEl.appendChild(Svg);
+      } catch (err) {
+        OutputEl.textContent = err instanceof Error ? err.message : String(err);
       }
     });
-  });
-
-  Form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    OutputEl.innerHTML = "";
-    const Operands = Inputs.map((el) => parseFloat(el.value)) as [
-        number,
-        number,
-        number,
-        number
-      ],
-      OPCODE = OpSelect.value;
-    let result: number;
-    switch (OPCODE) {
-      case "add":
-        result = cadd(...Operands);
-        break;
-      case "sub":
-        result = csub(...Operands);
-        break;
-      case "mul":
-        result = cmul(...Operands);
-        break;
-      case "div":
-        result = cdiv(...Operands);
-        break;
-      default:
-        throw "Invalid option";
-    }
-    const ResultArr = getArr(result),
-      RESULT_STR: string = formatComplex(...ResultArr),
-      Svg: HTMLElement = MathJax.tex2svg(RESULT_STR);
-    OutputEl.appendChild(Svg);
-  });
+  } catch (err) {
+    OutputEl.textContent = err instanceof Error ? err.message : String(err);
+  }
 }
 
-window.onload = main;
+window.addEventListener("load", main);
